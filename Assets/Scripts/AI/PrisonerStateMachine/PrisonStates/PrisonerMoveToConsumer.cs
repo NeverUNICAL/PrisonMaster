2a03a0b8c36b72d0ab490096af4c653bdb82abc5
NeverUNICAL <kesarev0@mail.ru>
2022-07-11 19:28:08 +0300

    NavMeshAgentToAssistant

--- Assets/Scripts/AI/PrisonerStateMachine/PrisonStates/PrisonerMoveToConsumer.cs
+++ Assets/Scripts/AI/PrisonerStateMachine/PrisonStates/PrisonerMoveToConsumer.cs
@@ -1,14 +1,23 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class PrisonerMoveToConsumer : PrisonerState
 {
     [SerializeField] private float _speed;
+    private NavMeshAgent _navMeshAgent;
+
+    private void Start()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+    }
 
     private void Update()
     {
-        transform.position = Vector3.MoveTowards(transform.position, ConsumerTarget.transform.position, _speed * Time.deltaTime);
-        transform.LookAt(ConsumerTarget.transform.position);
+        // transform.position = Vector3.MoveTowards(transform.position, ConsumerTarget.transform.position, _speed * Time.deltaTime);
+        _navMeshAgent.SetDestination(ConsumerTarget.transform.position);
+       // transform.LookAt(ConsumerTarget.transform.position);
     }
 }
