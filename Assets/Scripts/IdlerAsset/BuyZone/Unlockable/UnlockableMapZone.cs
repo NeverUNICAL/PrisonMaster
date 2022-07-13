e6c323d4b841ab596beca436e903566cd06f3ff1
NeverUNICAL <kesarev0@mail.ru>
2022-07-13 22:03:04 +0300

    MapUnlock

--- Assets/Scripts/IdlerAsset/BuyZone/Unlockable/UnlockableMapZone.cs
+++ Assets/Scripts/IdlerAsset/BuyZone/Unlockable/UnlockableMapZone.cs
@@ -5,9 +5,13 @@ namespace Agava.IdleGame
     public class UnlockableMapZone : UnlockableObject
     {
         [SerializeField] private GameObject _mapRoot;
+        [SerializeField] private GameObject _objectForTurnOff;
 
         public override GameObject Unlock(Transform parent, bool onLoad, string guid)
         {
+            if(_objectForTurnOff != null)
+                _objectForTurnOff.SetActive(false);
+            
             _mapRoot.SetActive(true);
             return _mapRoot;
         }
