bb7a9cdf5a4e741d7400ca42073248a784440af3
PreWis-D <79226367+PreWis-D@users.noreply.github.com>
2022-08-25 19:27:21 +0300

    EditBusPrefab

--- Assets/Bus.cs
+++ Assets/Bus.cs
@@ -10,103 +10,98 @@ using UnityEngine.AI;
 [RequireComponent(typeof(NavMeshAgent))]
 public class Bus : MonoBehaviour
 {
-   [SerializeField] private Trigger _trigger;
-   [SerializeField] private Transform _travelPoint;
-   [SerializeField] private float _timeBeforePressBrake = 2.2f;
-   [SerializeField] private float _timeToCloseDoor = 3f;
-   [SerializeField] private float _timeToRefreshPosition = 1f;
-   [SerializeField] private float _breakPower = 0.05f;
-   [SerializeField] private float _timeToRefreshWay = 5f;
-   [SerializeField] private StartPool _startPool;
-   [SerializeField] private ParticleSystem _fadingParticleSystem;
-   [SerializeField] private ParticleSystem _leftSmoke;
-   [SerializeField] private ParticleSystem _rightSmoke;
+    [SerializeField] private Trigger _trigger;
+    [SerializeField] private Transform _travelPoint;
+    [SerializeField] private Transform _outPoint;
+    [SerializeField] private float _timeToCloseDoor = 3f;
+    [SerializeField] private float _breakPower = 0.05f;
+    [SerializeField] private float _timeToRefreshWay = 5f;
+    [SerializeField] private StartPool _startPool;
+    [SerializeField] private ParticleSystem _leftSmoke;
+    [SerializeField] private ParticleSystem _rightSmoke;
 
-   private NavMeshAgent _agent;
-   private float _timeToDestroyFadingParticle = 2f;
-   private MeshRenderer _meshRenderer;
-   private Vector3 _startPosition;
-   private bool _brakePressed;
-   private bool _doorOpened;
-   private float _startSpeed;
-   private bool _isFirstTriggerEnter;
-   private ParticleSystem _instantiatedParticleSystem;
+    private NavMeshAgent _agent;
+    private MeshRenderer _meshRenderer;
+    private Vector3 _startPosition;
+    private bool _brakePressed;
+    private bool _doorOpened;
+    private float _startSpeed;
 
-   private void Start()
-   {
-      _meshRenderer = GetComponent<MeshRenderer>();
-      _agent = GetComponent<NavMeshAgent>();
-      _startPosition = transform.position;
-      _startSpeed = _agent.speed;
-   }
+    private void OnEnable()
+    {
+        _trigger.Enter += OnEnter;
+    }
 
-   private void Update()
-   {
-      if (_brakePressed && _agent.remainingDistance <= _agent.stoppingDistance && _doorOpened == false)
-      {
-         _doorOpened = true;
-         OpenDoor();
-      }
+    private void OnDisable()
+    {
+        _trigger.Enter -= OnEnter;
+    }
 
-      if (_brakePressed && _agent.speed > 1)
-         _agent.speed -= _breakPower * Time.deltaTime;
-   }
+    private void Start()
+    {
+        _meshRenderer = GetComponent<MeshRenderer>();
+        _agent = GetComponent<NavMeshAgent>();
+        _startPosition = transform.position;
+        _startSpeed = _agent.speed;
+    }
 
-   private void OnEnable()
-   {
-      _trigger.Enter += OnEnter;
-   }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent(out Trigger trigger))
+            OnBrake();
+    }
 
-   private void OnDisable()
-   {
-      _trigger.Enter -= OnEnter;
-   }
+    private void Update()
+    {
+        if (_brakePressed && _agent.remainingDistance <= _agent.stoppingDistance && _doorOpened == false)
+        {
+            _doorOpened = true;
+            OpenDoor();
+        }
 
-   private void OnEnter()
-   {
-      _trigger.Enter -= OnEnter;
-      _agent.SetDestination(_travelPoint.position);
-      Invoke(nameof(PressBrake),_timeBeforePressBrake);
-     
-   }
+        if (_brakePressed && _agent.speed > 1)
+            _agent.speed -= _breakPower * Time.deltaTime;
+    }
 
-   private void OpenDoor()
-   {
-      _startPool.ChangeSpawningState(true);
-      _brakePressed = false;
-      Invoke(nameof(CloseDoor),_timeToCloseDoor);
-   }
 
-   private void CloseDoor()
-   {
-      _startPool.ChangeSpawningState(false);
-      _doorOpened = false;
-      Invoke(nameof(RefreshPosition),_timeToRefreshPosition);
-      Invoke(nameof(RefreshWay),_timeToRefreshWay);
-   }
+    private void OnEnter()
+    {
+        _trigger.Enter -= OnEnter;
+        _agent.SetDestination(_travelPoint.position);
+    }
 
-   private void RefreshPosition()
-   {
-      _leftSmoke.gameObject.SetActive(false);
-      _rightSmoke.gameObject.SetActive(false);
-      _instantiatedParticleSystem = Instantiate(_fadingParticleSystem, transform);
-      _instantiatedParticleSystem.gameObject.transform.SetParent(null);
-      Destroy(_instantiatedParticleSystem.gameObject,_timeToDestroyFadingParticle);
-      transform.position = _startPosition;
-      _agent.SetDestination(_startPosition);
-   }
+    private void OpenDoor()
+    {
+        _startPool.ChangeSpawningState(true);
+        _brakePressed = false;
+        Invoke(nameof(CloseDoor), _timeToCloseDoor);
+    }
 
-   private void RefreshWay()
-   {
-      _agent.speed = _startSpeed;
-      _agent.SetDestination(_travelPoint.position);
-      Invoke(nameof(PressBrake),_timeBeforePressBrake);
-   }
+    private void CloseDoor()
+    {
+        _startPool.ChangeSpawningState(false);
+        _doorOpened = false;
+        ChangeSmokeActive(false);
+        _agent.speed = _startSpeed;
+        _agent.SetDestination(_outPoint.position);
+        Invoke(nameof(RefreshWay), _timeToRefreshWay);
+    }
 
-   private void PressBrake()
-   {
-      _leftSmoke.gameObject.SetActive(true);
-      _rightSmoke.gameObject.SetActive(true);
-      _brakePressed = true;
-   }
+    private void RefreshWay()
+    {
+        transform.position = _startPosition;
+        _agent.SetDestination(_travelPoint.position);
+    }
+
+    private void OnBrake()
+    {
+        ChangeSmokeActive(true);
+        _brakePressed = true;
+    }
+
+    private void ChangeSmokeActive(bool value)
+    {
+        _leftSmoke.gameObject.SetActive(value);
+        _rightSmoke.gameObject.SetActive(value);
+    }
 }
