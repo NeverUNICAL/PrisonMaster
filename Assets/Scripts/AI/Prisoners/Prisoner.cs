cb882dad87bf34f5fe77377a2e2bcaa533a8c497
PreWis-D <79226367+PreWis-D@users.noreply.github.com>
2022-09-08 10:05:05 +0300

    EditScene

--- Assets/Scripts/AI/Prisoners/Prisoner.cs
+++ Assets/Scripts/AI/Prisoners/Prisoner.cs
@@ -1,8 +1,22 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Prisoner : MonoBehaviour
 {
-    
+    private NavMeshAgent _navMeshAgent;
+
+    public NavMeshAgent NavMeshAgent => _navMeshAgent;
+
+    private void Awake()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    public void Rotate(Transform target, float duration = 1f)
+    {
+        transform.DORotateQuaternion(target.rotation, duration);
+    }
 }
